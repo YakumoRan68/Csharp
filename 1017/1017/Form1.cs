15c96b53ba0c2d4c67b9b623572aec75e8db349f
YakumoRan <yys14239103@gmail.com>
2018-10-17 16:21:16 +0900

    1017

---
+++ 1017/1017/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _1017
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeSign(int a) {
+            switch(a)
+            {
+                case 1: pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/../../Images/red.ico"); break;
+                case 2: pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/../../Images/yellow.ico"); break;
+                case 3: pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/../../Images/blue.ico"); break;
+                default: return;
+            }
+        }  
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeSign(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChangeSign(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChangeSign(3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChangeSign(3);
+        }
+    }
+}
---
+++ 1017/1017/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1017
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        int t = 1;
+        bool delta = true;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (t)
+            {
+                case 1: pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + "/../../Images/red.ico"); break;
+                case 2: pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + "/../../Images/yellow.ico"); break;
+                case 3: pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + "/../../Images/blue.ico"); break;
+            }
+
+            t = delta ? ++t : --t;
+            delta = t == 3 || t == 1 ? !delta : delta;
+        }
+    }
+}
