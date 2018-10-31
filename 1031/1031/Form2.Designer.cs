886ad4ba0e59bf471f7f0ac05e051176fd5a8d31
YakumoRan <37825675+YakumoRan68@users.noreply.github.com>
2018-10-31 15:13:56 +0900

    1031

---
+++ 1031/1031/Form2.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace _1031
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 276);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1번 사진";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3번 사진";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2번 사진";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 316);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ 1031/1031/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1031
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int tick = 1, type = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ChangeImg(tick);
+            tick++;
+        }
+
+        public void ChangeImg(int a)
+        {
+            string Dir = "/../../Images/";
+            /*switch (a) {
+                case 1: pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + Dir + "1/1.jpg"); break;
+                case 2: pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + Dir + "1/2.jpg"); break;
+                case 3: pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + Dir + "1/3.jpg"); break;
+                case 4: pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + Dir + "1/4.jpg"); break;
+            }*/
+            int frame = a;
+            try
+            {
+                pictureBox1.Image = Image.FromFile(Environment.CurrentDirectory + Dir + Convert.ToString(type)+ "/" + Convert.ToString(frame) + ".jpg" );
+            }
+            catch(System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("file not found");
+                return;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            type = 3;
+            tick = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            type = 2;
+            tick = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            type = 1;
+            tick = 0;
+        }
+    }
+}
