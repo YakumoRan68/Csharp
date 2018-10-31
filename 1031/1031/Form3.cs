6b01e512383c2efbad4f218971d8b71db5fe8188
YakumoRan <37825675+YakumoRan68@users.noreply.github.com>
2018-10-31 16:24:08 +0900

    설명 추가
    교수님이 잊어버리지 말랩니다

--- 1031/1031/Form3.cs
+++ 1031/1031/Form3.cs
@@ -20,9 +20,11 @@ namespace _1031
         int tick = 1;
         private void timer1_Tick(object sender, EventArgs e)
         {
-            for (int i = 1; i <= 3; i++) { //pictureBox1, 2, 3의 컨트롤을 가져오기 위해 사용
-                var count = Directory.GetFiles(@Environment.CurrentDirectory + "/../../Images/" + i.ToString() + "/", "*.jpg").Length; // 해당 이름을 가진 디렉토리의 파일 갯수
+            for (int i = 1; i <= 3; i++) {
+                int count = Directory.GetFiles(@Environment.CurrentDirectory + "/../../Images/" + i.ToString() + "/", "*.jpg").Length; // 해당 이름을 가진 디렉토리의 파일의 리스트(자료형 list).갯수
                 ((PictureBox)Controls["pictureBox" + i.ToString()]).Image = Image.FromFile(Environment.CurrentDirectory + "/../../Images/" + i.ToString() + "/" + Convert.ToString((tick % count) + 1) + ".jpg");
+                //(PictureBox) : Controls[]로 컨트롤을 묶음으로 가져올 수 있고 PictureBox도 컨트롤이나, PictureBox의 Image 매서드를 호출할 경우에는 명시적인 형변환이 필요함.
+                //문자열으로의 명시적인 형변환의 종류 : Convert.ToString(i), i.ToString
             }
             tick++;
         }
