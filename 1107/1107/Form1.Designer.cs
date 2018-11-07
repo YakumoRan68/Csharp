088c1c7bb016aab9c2b5492c580a37ee8cd448dd
YakumoYukari <HimekaidouHatate@users.noreply.github.com>
2018-11-07 16:16:42 +0900

    1107

---
+++ 1107/1107/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace _1107
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NUM1 = new System.Windows.Forms.Button();
+            this.NUM2 = new System.Windows.Forms.Button();
+            this.NUM3 = new System.Windows.Forms.Button();
+            this.NUM0 = new System.Windows.Forms.Button();
+            this.Dot = new System.Windows.Forms.Button();
+            this.NUM7 = new System.Windows.Forms.Button();
+            this.NUM8 = new System.Windows.Forms.Button();
+            this.NUM9 = new System.Windows.Forms.Button();
+            this.NUM4 = new System.Windows.Forms.Button();
+            this.NUM5 = new System.Windows.Forms.Button();
+            this.NUM6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AC = new System.Windows.Forms.Button();
+            this.CE = new System.Windows.Forms.Button();
+            this.Backspace = new System.Windows.Forms.Button();
+            this.PM = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.Screen = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NUM00 = new System.Windows.Forms.Button();
+            this.MemoryScreen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NUM1
+            // 
+            this.NUM1.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM1.Location = new System.Drawing.Point(12, 262);
+            this.NUM1.Name = "NUM1";
+            this.NUM1.Size = new System.Drawing.Size(61, 55);
+            this.NUM1.TabIndex = 1;
+            this.NUM1.Text = "1";
+            this.NUM1.UseVisualStyleBackColor = true;
+            this.NUM1.Click += new System.EventHandler(this.NUM1_Click);
+            // 
+            // NUM2
+            // 
+            this.NUM2.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM2.Location = new System.Drawing.Point(72, 262);
+            this.NUM2.Name = "NUM2";
+            this.NUM2.Size = new System.Drawing.Size(61, 55);
+            this.NUM2.TabIndex = 2;
+            this.NUM2.Text = "2";
+            this.NUM2.UseVisualStyleBackColor = true;
+            this.NUM2.Click += new System.EventHandler(this.NUM2_Click);
+            // 
+            // NUM3
+            // 
+            this.NUM3.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM3.Location = new System.Drawing.Point(132, 262);
+            this.NUM3.Name = "NUM3";
+            this.NUM3.Size = new System.Drawing.Size(61, 55);
+            this.NUM3.TabIndex = 3;
+            this.NUM3.Text = "3";
+            this.NUM3.UseVisualStyleBackColor = true;
+            this.NUM3.Click += new System.EventHandler(this.NUM3_Click);
+            // 
+            // NUM0
+            // 
+            this.NUM0.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM0.Location = new System.Drawing.Point(12, 323);
+            this.NUM0.Name = "NUM0";
+            this.NUM0.Size = new System.Drawing.Size(121, 55);
+            this.NUM0.TabIndex = 1;
+            this.NUM0.Text = "0";
+            this.NUM0.UseVisualStyleBackColor = true;
+            this.NUM0.Click += new System.EventHandler(this.NUM0_Click);
+            // 
+            // Dot
+            // 
+            this.Dot.Font = new System.Drawing.Font("굴림", 11F);
+            this.Dot.Location = new System.Drawing.Point(132, 323);
+            this.Dot.Name = "Dot";
+            this.Dot.Size = new System.Drawing.Size(61, 55);
+            this.Dot.TabIndex = 2;
+            this.Dot.Text = ".";
+            this.Dot.UseVisualStyleBackColor = true;
+            this.Dot.Click += new System.EventHandler(this.Dot_Click);
+            // 
+            // NUM7
+            // 
+            this.NUM7.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM7.Location = new System.Drawing.Point(12, 140);
+            this.NUM7.Name = "NUM7";
+            this.NUM7.Size = new System.Drawing.Size(61, 55);
+            this.NUM7.TabIndex = 1;
+            this.NUM7.Text = "7";
+            this.NUM7.UseVisualStyleBackColor = true;
+            this.NUM7.Click += new System.EventHandler(this.NUM7_Click);
+            // 
+            // NUM8
+            // 
+            this.NUM8.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM8.Location = new System.Drawing.Point(72, 140);
+            this.NUM8.Name = "NUM8";
+            this.NUM8.Size = new System.Drawing.Size(61, 55);
+            this.NUM8.TabIndex = 2;
+            this.NUM8.Text = "8";
+            this.NUM8.UseVisualStyleBackColor = true;
+            this.NUM8.Click += new System.EventHandler(this.NUM8_Click);
+            // 
+            // NUM9
+            // 
+            this.NUM9.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM9.Location = new System.Drawing.Point(132, 140);
+            this.NUM9.Name = "NUM9";
+            this.NUM9.Size = new System.Drawing.Size(61, 55);
+            this.NUM9.TabIndex = 3;
+            this.NUM9.Text = "9";
+            this.NUM9.UseVisualStyleBackColor = true;
+            this.NUM9.Click += new System.EventHandler(this.NUM9_Click);
+            // 
+            // NUM4
+            // 
+            this.NUM4.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM4.Location = new System.Drawing.Point(12, 201);
+            this.NUM4.Name = "NUM4";
+            this.NUM4.Size = new System.Drawing.Size(61, 55);
+            this.NUM4.TabIndex = 1;
+            this.NUM4.Text = "4";
+            this.NUM4.UseVisualStyleBackColor = true;
+            this.NUM4.Click += new System.EventHandler(this.NUM4_Click);
+            // 
+            // NUM5
+            // 
+            this.NUM5.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM5.Location = new System.Drawing.Point(72, 201);
+            this.NUM5.Name = "NUM5";
+            this.NUM5.Size = new System.Drawing.Size(61, 55);
+            this.NUM5.TabIndex = 2;
+            this.NUM5.Text = "5";
+            this.NUM5.UseVisualStyleBackColor = true;
+            this.NUM5.Click += new System.EventHandler(this.NUM5_Click);
+            // 
+            // NUM6
+            // 
+            this.NUM6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.NUM6.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM6.Location = new System.Drawing.Point(132, 201);
+            this.NUM6.Name = "NUM6";
+            this.NUM6.Size = new System.Drawing.Size(61, 55);
+            this.NUM6.TabIndex = 3;
+            this.NUM6.Text = "6";
+            this.NUM6.UseVisualStyleBackColor = true;
+            this.NUM6.Click += new System.EventHandler(this.NUM6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // AC
+            // 
+            this.AC.Font = new System.Drawing.Font("굴림", 11F);
+            this.AC.Location = new System.Drawing.Point(132, 79);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(61, 55);
+            this.AC.TabIndex = 5;
+            this.AC.Text = "AC";
+            this.AC.UseVisualStyleBackColor = true;
+            this.AC.Click += new System.EventHandler(this.AC_Click);
+            // 
+            // CE
+            // 
+            this.CE.Font = new System.Drawing.Font("굴림", 11F);
+            this.CE.Location = new System.Drawing.Point(72, 79);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(61, 55);
+            this.CE.TabIndex = 6;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // Backspace
+            // 
+            this.Backspace.Font = new System.Drawing.Font("굴림", 11F);
+            this.Backspace.Location = new System.Drawing.Point(12, 79);
+            this.Backspace.Name = "Backspace";
+            this.Backspace.Size = new System.Drawing.Size(61, 55);
+            this.Backspace.TabIndex = 7;
+            this.Backspace.Text = "←";
+            this.Backspace.UseVisualStyleBackColor = true;
+            this.Backspace.Click += new System.EventHandler(this.Backspace_Click);
+            // 
+            // PM
+            // 
+            this.PM.Font = new System.Drawing.Font("굴림", 11F);
+            this.PM.Location = new System.Drawing.Point(208, 79);
+            this.PM.Name = "PM";
+            this.PM.Size = new System.Drawing.Size(61, 55);
+            this.PM.TabIndex = 12;
+            this.PM.Text = "±";
+            this.PM.UseVisualStyleBackColor = true;
+            // 
+            // Multiply
+            // 
+            this.Multiply.Font = new System.Drawing.Font("굴림", 11F);
+            this.Multiply.Location = new System.Drawing.Point(208, 201);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(61, 55);
+            this.Multiply.TabIndex = 9;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("굴림", 11F);
+            this.divide.Location = new System.Drawing.Point(208, 140);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(61, 55);
+            this.divide.TabIndex = 10;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            // 
+            // Plus
+            // 
+            this.Plus.Font = new System.Drawing.Font("굴림", 11F);
+            this.Plus.Location = new System.Drawing.Point(208, 323);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(61, 55);
+            this.Plus.TabIndex = 8;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Font = new System.Drawing.Font("굴림", 11F);
+            this.Minus.Location = new System.Drawing.Point(208, 262);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(61, 55);
+            this.Minus.TabIndex = 11;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            // 
+            // Equal
+            // 
+            this.Equal.Font = new System.Drawing.Font("굴림", 11F);
+            this.Equal.Location = new System.Drawing.Point(275, 262);
+            this.Equal.Name = "Equal";
+            this.Equal.Size = new System.Drawing.Size(61, 116);
+            this.Equal.TabIndex = 13;
+            this.Equal.Text = "=";
+            this.Equal.UseVisualStyleBackColor = true;
+            // 
+            // Screen
+            // 
+            this.Screen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Screen.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Screen.Font = new System.Drawing.Font("굴림", 14F);
+            this.Screen.Location = new System.Drawing.Point(14, 39);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(322, 19);
+            this.Screen.TabIndex = 14;
+            this.Screen.Text = "                                                    ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 46);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // NUM00
+            // 
+            this.NUM00.Font = new System.Drawing.Font("굴림", 11F);
+            this.NUM00.Location = new System.Drawing.Point(132, 384);
+            this.NUM00.Name = "NUM00";
+            this.NUM00.Size = new System.Drawing.Size(61, 39);
+            this.NUM00.TabIndex = 16;
+            this.NUM00.Text = "00";
+            this.NUM00.UseVisualStyleBackColor = true;
+            // 
+            // MemoryScreen
+            // 
+            this.MemoryScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MemoryScreen.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MemoryScreen.Font = new System.Drawing.Font("굴림", 14F);
+            this.MemoryScreen.Location = new System.Drawing.Point(12, 12);
+            this.MemoryScreen.Name = "MemoryScreen";
+            this.MemoryScreen.Size = new System.Drawing.Size(322, 19);
+            this.MemoryScreen.TabIndex = 17;
+            this.MemoryScreen.Text = "                                                    ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 425);
+            this.Controls.Add(this.MemoryScreen);
+            this.Controls.Add(this.NUM00);
+            this.Controls.Add(this.Screen);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.PM);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Backspace);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.AC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NUM6);
+            this.Controls.Add(this.NUM5);
+            this.Controls.Add(this.NUM9);
+            this.Controls.Add(this.NUM8);
+            this.Controls.Add(this.NUM4);
+            this.Controls.Add(this.Dot);
+            this.Controls.Add(this.NUM7);
+            this.Controls.Add(this.NUM3);
+            this.Controls.Add(this.NUM0);
+            this.Controls.Add(this.NUM2);
+            this.Controls.Add(this.NUM1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "계산기";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button NUM1;
+        private System.Windows.Forms.Button NUM2;
+        private System.Windows.Forms.Button NUM3;
+        private System.Windows.Forms.Button NUM0;
+        private System.Windows.Forms.Button Dot;
+        private System.Windows.Forms.Button NUM7;
+        private System.Windows.Forms.Button NUM8;
+        private System.Windows.Forms.Button NUM9;
+        private System.Windows.Forms.Button NUM4;
+        private System.Windows.Forms.Button NUM5;
+        private System.Windows.Forms.Button NUM6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AC;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button Backspace;
+        private System.Windows.Forms.Button PM;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Equal;
+        private System.Windows.Forms.Label Screen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button NUM00;
+        private System.Windows.Forms.Label MemoryScreen;
+    }
+}
+
